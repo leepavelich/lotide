8d8aa6ee36baf6f57e2db324ec72f57da1c82403
Lee Pavelich <leepavelich@gmail.com>
2022-01-10 23:18:06 +0000

    Implemented and tested flatten

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = arr => {
+  let newArr = [];
+  for (let elem of arr) {
+    if (!Array.isArray(elem)) {
+      newArr.push(elem);
+    } else {
+      for (let subelem of elem) {
+        newArr.push(subelem);
+      }
+    }
+  }
+  return newArr;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
