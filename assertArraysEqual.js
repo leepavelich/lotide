16eec3967f4dfdcb936d74b4a96adec6dde9fd3a
Lee Pavelich <leepavelich@gmail.com>
2022-01-10 22:47:14 +0000

    Implemented and tested assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TESTS
+// assertArraysEqual([1, 2, 3], [1, 2, 3])
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
