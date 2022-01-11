c0add63da5087d17a0d68a2a50f7220cdc4388e3
Lee Pavelich <leepavelich@gmail.com>
2022-01-11 18:27:28 +0000

    Implemented and tested map

---
+++ map.js
@@ -0,0 +1,11 @@
+const map = (array, func) => {
+  let mapped = []
+  for (let elem of array) {
+    mapped.push(func(elem))
+  }
+  return mapped
+}
+
+// TESTS
+const words = ["ground", "control", "to", "major", "tom"];
+console.log(map(words, word => word[0]))
