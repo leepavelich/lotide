489daec4f7c07db5eb1a76ab9ab8a37ef39125e9
Lee Pavelich <leepavelich@gmail.com>
2022-01-11 00:29:02 +0000

    Placed missing brackets in output message for arrays

--- without.js
+++ without.js
@@ -13,9 +13,9 @@ const eqArrays = (actual, expected) => {
 
 const assertArraysEqual = (actual, expected) => {
   if (eqArrays(actual,expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 
