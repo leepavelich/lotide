8336eb41ea0a3028e1c1ecd6587fc2b277df3638
Lee Pavelich <leepavelich@gmail.com>
2022-01-11 00:27:38 +0000

    Initialized middle

---
+++ middle.js
@@ -0,0 +1,26 @@
+const eqArrays = (actual, expected) => {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const middle = arr => {
+  if (arr.length === 1 || arr.length === 2) return []
+}
+
+assertArraysEqual(middle([1]), [])
