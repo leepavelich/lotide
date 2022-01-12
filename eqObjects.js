72a65d29c40cba9d9f43229e9df6887bfdd2401d
Lee Pavelich <leepavelich@gmail.com>
2022-01-12 19:49:22 +0000

    Fixed false report of true if first elements were equal

--- eqObjects.js
+++ eqObjects.js
@@ -20,37 +20,38 @@ const eqArrays = (actual, expected) => {
 };
 
 const eqObjects = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+  if (object1 === object2) return true
+
+  if (typeof object1 !== "object" || typeof object2 !== "object") return false
+
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
 
   if (keys1.length !== keys2.length) return false;
+
   for (let key of keys1) {
-    if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
-      if ((typeof object1[key] === "object") || (typeof object2[key] === "object")) {
-        return eqObjects (object1[key], object2[key])
-      }
-    }
-    if (!eqArrays(object1[key], object2[key])) return false;
+    if (!keys2.includes(key) || !eqObjects(object1[key], object2[key])) return false
   }
   return true;
 };
 
 // TESTS
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true);
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false);
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true);
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false);
 
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)
-assertEqual(eqObjects({ a: { y: 0, z: { x: 5, w: 6} }, b: 2 }, { a: 1, b: { x: 3, w: 6} }), false)
+// assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true)
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)
+// assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false)
+// assertEqual(eqObjects({ a: { y: 0, z: { x: 5, w: 6} }, b: 2 }, { a: 1, b: { x: 3, w: 6} }), false)
+// assertEqual(eqObjects({a: {b: 3, c: 4}, d: 5}, {a: {b: 3, c: 4}, d: 6}),false)
