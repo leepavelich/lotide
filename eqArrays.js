d60432118e4ac513c94fab95e5eea84a9e54e01b
Lee Pavelich <leepavelich@gmail.com>
2022-01-12 19:54:27 +0000

    Can now handled nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,20 +7,24 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (actual, expected) => {
-  if (actual.length !== expected.length) {
-    return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
+  if (actual === expected) return true
+
+  if (!Array.isArray(actual) || !Array.isArray(expected)) return false
+
+  if (actual.length !== expected.length) return false;
+
+  for (let i = 0; i < actual.length; i++) {
+    if (!eqArrays (actual[i], expected[i])) return false
   }
   return true;
 };
 
 // TESTS
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true)
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),true)
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true)
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) ,true)
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) ,false)
+
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
