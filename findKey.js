38625d253fd511b680abfdf7b92b6d56e145d24a
Lee Pavelich <leepavelich@gmail.com>
2022-01-15 05:25:13 +0000

    Commented out test code

--- findKey.js
+++ findKey.js
@@ -12,11 +12,12 @@ const findKey = (obj, func) => {
   }
 }
 
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), 'noma') // => "noma"
+// TESTS
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2), 'noma')
