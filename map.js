130383d8f25fd12b950661913a981adc325598f2
Lee Pavelich <leepavelich@gmail.com>
2022-01-15 05:58:54 +0000

    Commented out test code

--- map.js
+++ map.js
@@ -7,5 +7,5 @@ const map = (array, func) => {
 }
 
 // TESTS
-const words = ["ground", "control", "to", "major", "tom"];
-console.log(map(words, word => word[0]))
+// const words = ["ground", "control", "to", "major", "tom"];
+// console.log(map(words, word => word[0]))
