feaf2263c4d20a4c39dbd8ce4774600c0048f847
Lee Pavelich <leepavelich@gmail.com>
2022-01-15 06:01:58 +0000

    Refactored

--- middle.js
+++ middle.js
@@ -20,17 +20,15 @@ const assertArraysEqual = (actual, expected) => {
 };
 
 const middle = arr => {
-  if (arr.length === 1 || arr.length === 2) return []
-  if (arr.length % 2 !== 0) {
-    return [arr[Math.floor(arr.length/2)]]
-  } else {
-    return [arr[arr.length/2 - 1], arr[arr.length/2]]
-  }
-}
+  if (arr.length === 1 || arr.length === 2) return [];
+  if (arr.length % 2 !== 0) return [arr[Math.floor(arr.length / 2)]]; // odd length
+  return [arr[arr.length / 2 - 1], arr[arr.length / 2]]; // even length
+};
 
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+// TESTS
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
