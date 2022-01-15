481a3f72e5b2881086dabee93db9c1818ecb7a8e
Lee Pavelich <leepavelich@gmail.com>
2022-01-15 05:05:06 +0000

    Linted

--- eqArrays.js
+++ eqArrays.js
@@ -7,12 +7,12 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (actual, expected) => {
-  if (actual === expected) return true
-  if (!Array.isArray(actual) || !Array.isArray(expected)) return false
+  if (actual === expected) return true;
+  if (!Array.isArray(actual) || !Array.isArray(expected)) return false;
   if (actual.length !== expected.length) return false;
   
   for (let i = 0; i < actual.length; i++) {
-    if (!eqArrays (actual[i], expected[i])) return false
+    if (!eqArrays(actual[i], expected[i])) return false;
   }
   return true;
 };
