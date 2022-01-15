7c937759e2768e8f6ef48a2e15ae25fa51a436b7
Lee Pavelich <leepavelich@gmail.com>
2022-01-15 05:45:12 +0000

    Commented out test code

--- head.js
+++ head.js
@@ -8,6 +8,7 @@ const assertEqual = (actual, expected) => {
 
 const head = arr => arr[0];
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+// TESTS
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), undefined);
