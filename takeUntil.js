79d606833413757fcfe14669cb2e38d905a1b4d2
Lee Pavelich <leepavelich@gmail.com>
2022-01-15 06:03:39 +0000

    Refactored

--- takeUntil.js
+++ takeUntil.js
@@ -1,22 +1,19 @@
 const takeUntil = (array, callback) => {
   let taken = [];
   for (let item of array) {
-    if (!callback(item)) {
-      taken.push(item);
-    } else {
-      break;
-    }
+    if (callback(item)) break;
+    taken.push(item);
   }
   return taken;
 };
 
 // TESTS
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
 
-console.log('---');
+// console.log('---');
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
