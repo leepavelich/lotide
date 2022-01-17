2e7975aa909afe1a91d59032ddacf34e24ec2749
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 04:19:59 +0000

    Separated out test code from assertEqual and head

--- assertEqual.js
+++ assertEqual.js
@@ -4,6 +4,4 @@ const assertEqual = (actual, expected) => {
     : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
+module.exports = assertEqual;
