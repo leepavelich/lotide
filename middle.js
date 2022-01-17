99a6fcfc7405235f605a5cafab7583ab7e8830cb
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 16:55:46 +0000

    Removed legacy reference

--- middle.js
+++ middle.js
@@ -1,5 +1,3 @@
-const eqArrays = require('./eqArrays');
-
 const middle = arr => {
   if (arr.length === 1 || arr.length === 2) return [];
   if (arr.length % 2 !== 0) return [arr[Math.floor(arr.length / 2)]]; // odd length
