c939f93fd4eb2e07732e81f471e1dc8b93913fc3
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 17:17:09 +0000

    Moved findKey test code to dedicated file with mocha chai

--- findKey.js
+++ findKey.js
@@ -1,13 +1,3 @@
 const findKey = (obj, func) => Object.keys(obj).find(k => func(obj[k]));
 
 module.exports = findKey;
-
-// TESTS
-// assertEqual(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2), 'noma')
