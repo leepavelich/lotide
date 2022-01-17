8fa74a8771de9d3250a924afb8f34589d6ff7ffb
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 04:30:41 +0000

    Renamed test code area

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays')
 
-// TESTS
+// TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true)
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true)
