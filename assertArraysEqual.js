160632c2ab03828a9844b836d079f6733382cb4e
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 04:30:54 +0000

    Refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = (actual, expected) => {
-  if (actual.length !== expected.length) {
-    return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = (actual, expected) => {
   console.log(eqArrays(actual, expected)
@@ -17,6 +6,4 @@ const assertArraysEqual = (actual, expected) => {
     : `🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
-// TESTS
-// assertArraysEqual([1, 2, 3], [1, 2, 3])
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3])
+module.exports = assertArraysEqual
