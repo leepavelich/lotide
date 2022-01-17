ae9dbe6fe864223821f450b7096df3cfe8edbdc4
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 04:26:45 +0000

    Refactored eqArrays to move tests to dedicated file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (actual, expected) => {
   if (actual === expected) return true;
   if (!Array.isArray(actual) || !Array.isArray(expected)) return false;
@@ -17,12 +9,5 @@ const eqArrays = (actual, expected) => {
   return true;
 };
 
-// TESTS
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true)
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true)
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]) ,false)
+module.exports = eqArrays
 
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)
