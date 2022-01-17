87ceab46eda14b9fef3834223277ffcf57bda0d3
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 04:51:17 +0000

    Rewrote tailTest to use mocha chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(tail(words).length, 2);
+describe('#tail', () => {
+  it("returns length 2 for [\"Yo Yo\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
+  });
+  it("returns length 1 for [1,5]", () => {
+    assert.strictEqual(tail([1,5]).length, 1);
+  });
+});
