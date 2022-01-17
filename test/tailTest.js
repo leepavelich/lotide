f68f108a9300d7b6fd87d6b009bdfc3d08e7c561
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 04:52:14 +0000

    Rewrote one test to use deepEqual comparison

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,7 +5,7 @@ describe('#tail', () => {
   it("returns length 2 for [\"Yo Yo\", \"Lighthouse\", \"Labs\"]", () => {
     assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
   });
-  it("returns length 1 for [1,5]", () => {
-    assert.strictEqual(tail([1,5]).length, 1);
+  it("returns [5]] for [1,5]", () => {
+    assert.deepEqual(tail([1,5]), [5]);
   });
 });
