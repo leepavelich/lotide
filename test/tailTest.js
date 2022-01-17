8cc5d227b4b758c29e2349e4f5b5ea389d3c7dc1
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 17:25:03 +0000

    Test description fix

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,7 +5,7 @@ describe('#tail', () => {
   it("returns length 2 for [\"Yo Yo\", \"Lighthouse\", \"Labs\"]", () => {
     assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2);
   });
-  it("returns [5]] for [1,5]", () => {
+  it("returns [5] for [1,5]", () => {
     assert.deepEqual(tail([1,5]), [5]);
   });
 });
