10778517600ab8f2663b9374baf635e3f0b29d51
Lee Pavelich <leepavelich@gmail.com>
2022-01-17 17:20:17 +0000

    Moved findKeyByValue test code to dedicated file with mocha chai

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,3 @@
 const findKeyByValue = (obj, val) => Object.keys(obj).find(k => obj[k] === val);
 
 module.exports = findKeyByValue;
-
-// TESTS
-// const bestTVShowsByGenre = {
-//   sci_fi: "The Expanse",
-//   comedy: "Brooklyn Nine-Nine",
-//   drama:  "The Wire"
-// };
-
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
